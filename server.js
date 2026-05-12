50ef642c23aa623335f17b8415b9d5e115c5f011
bruno bergeret <bruberg@free.fr>
2026-05-12 20:51:07 +0200

    debug: afficher debut cle Brevo

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+console.log(
+  "BREVO_API_KEY reçue:",
+  process.env.BREVO_API_KEY
+    ? process.env.BREVO_API_KEY.substring(0, 10) + "..."
+    : "VIDE",
+);
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
